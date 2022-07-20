71d25a09641052b81cb20991207940e1c9da050d
BerseKIEL <exebarco280320@gmail.com>
2022-07-20 20:30:56 -0300

    Agregado lo fundamental

---
+++ app/static/js/main.js
@@ -0,0 +1,83 @@
+/* --- Home --- */
+/* === Constantes ===*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'), navClose = document.getElementById('nav-close');
+
+/* === Mostrar Menu===*/
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+        navToggle.style.display='none';
+    })
+}
+
+/* === Ocultar Menu===*/
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+        navToggle.style.display='block';
+    })
+}
+
+/* === Remover Menu al clickear === */
+const navLink = document.querySelectorAll('.nav__link')
+
+/* === Funcion sencilla que cuando al clickeas el menu se remueve === */
+function linkAction(){
+    
+    const navMenu = document.getElementById('nav-menu')
+    
+    navMenu.classList.remove('show-menu')
+
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+ 
+
+// /* ---- Habilidades --- */
+const tabs = document.querySelectorAll('[data-target]'), tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('habilidades__active')
+        })
+
+        target.classList.add('habilidades__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('habilidades__active')
+        })
+
+        tab.classList.add('habilidades__active')
+    })
+})
+
+
+/* === Cerrar y Abrir habilidades === */
+const ContHab = document.getElementsByClassName('habilidades__contenido__dos'), HeaderHab = document.querySelectorAll('.habilidades__header')
+
+function toggleHabilidad() {
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < ContHab.lenght; i++) {
+        ContHab[i].className = 'habilidades__contenido__dos habilidades__cerradas'
+    }
+    if(itemClass == 'habilidades__contenido__dos habilidades__cerradas') {
+        this.parentNode.className = 'habilidades__contenido__dos habilidades__abiertas'
+    }
+
+    else {
+        this.parentNode.className = 'habilidades__contenido__dos habilidades__cerradas'
+    }
+
+
+}
+
+HeaderHab.forEach((el) => {
+    el.addEventListener('click',toggleHabilidad)
+})
+
+
