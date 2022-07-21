4d9ce114b05cbc15f544349aaad58a22f14295a4
BerseKIEL <exebarco280320@gmail.com>
2022-07-21 08:57:44 -0300

    Base HTML casi finalizado

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -24,12 +24,11 @@ const navLink = document.querySelectorAll('.nav__link')
 
 /* === Funcion sencilla que cuando al clickeas el menu se remueve === */
 function linkAction(){
-    
     const navMenu = document.getElementById('nav-menu')
-    
     navMenu.classList.remove('show-menu')
-
+    navToggle.style.display='block';
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
  
@@ -80,4 +79,22 @@ HeaderHab.forEach((el) => {
     el.addEventListener('click',toggleHabilidad)
 })
 
+/* ------ Realiza scroll al link activo desde un ID ------ */
+const sections = document.querySelectorAll('seccion[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
